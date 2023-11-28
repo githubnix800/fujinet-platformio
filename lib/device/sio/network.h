e2b58588122750c27a91391725aada6c63b29e5a
Git Hubnix <93116152+githubnix800@users.noreply.github.com>
2023-11-28 15:27:31 +0100

    Update network.h

--- lib/device/sio/network.h
+++ lib/device/sio/network.h
@@ -6,14 +6,14 @@
 #include <string>
 #include <vector>
 
-#include "bus.h"
+#include "../bus/bus.h"
 
-#include "Protocol.h"
-#include "EdUrlParser.h"
-#include "networkStatus.h"
-#include "status_error_codes.h"
-#include "fnjson.h"
-#include "ProtocolParser.h"
+#include "../network-protocol/Protocol.h"
+#include "../EdUrlParser/EdUrlParser.h"
+#include "../network-protocol/networkStatus.h"
+#include "../network-protocol/status_error_codes.h"
+#include "../fnjson/fnjson.h"
+#include "../network-protocol/ProtocolParser.h"
 
 /**
  * Number of devices to expose via SIO, becomes 0x71 to 0x70 + NUM_DEVICES - 1
