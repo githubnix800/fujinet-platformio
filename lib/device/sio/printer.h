9343a2a729255b8efa77f57f6fda1899c8a8d40e
Git Hubnix <93116152+githubnix800@users.noreply.github.com>
2023-11-28 15:27:35 +0100

    Update printer.h

--- lib/device/sio/printer.h
+++ lib/device/sio/printer.h
@@ -4,8 +4,8 @@
 
 #include <string>
 
-#include "bus.h"
-#include "printer_emulator.h"
+#include "../bus/bus.h"
+#include "../printer-emulator/printer_emulator.h"
 #include "fnFS.h"
 
 #define PRINTER_UNSUPPORTED "Unsupported"
